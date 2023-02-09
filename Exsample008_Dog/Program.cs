bf6f47b5f99eedf593c0c752080d66ca43532fd9
Vyacheslav Kulikov <slavonmail10@gmail>
2023-02-09 11:00:43 +0200

    Написал программу по задачке 2 друга и собака

--- Exsample008_Dog/Program.cs
+++ Exsample008_Dog/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿int count = 0;
+int distance = 10000;
+int firstFriendspeed = 1;
+int secondFriendspeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int time = 0;
+
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstFriendspeed + dogSpeed);
+        friend = 2;
+    }
+
+    else
+    {
+        time = distance / (secondFriendspeed + dogSpeed);
+        friend = 1;
+    }
+
+    distance = distance - (firstFriendspeed + secondFriendspeed) * time;
+    count++;
+
+}
+Console.Write("count = ");
+Console.WriteLine(count);
