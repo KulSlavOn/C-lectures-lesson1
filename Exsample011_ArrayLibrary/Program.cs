144af26f225db74564a09eee3bc4c394e6150b1e
Vyacheslav Kulikov <slavonmail10@gmail>
2023-02-13 14:41:05 +0200

    Exsample009_IntroArray, Exsample009_MethodArray, Exsample009_ArrayLibrary

---
+++ Exsample011_ArrayLibrary/Program.cs
@@ -0,0 +1,52 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int postion = -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            postion = index;
+            break;
+        }
+        index++;
+    }
+    return postion;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+
+array[4] = 4;
+array[6] = 4;
+
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 44);
+Console.WriteLine(pos);
